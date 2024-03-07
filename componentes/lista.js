6eb3aa1540b06566652678e622d60c0ea3551a78
Victor Dias Noleto <lzero1541@gmail.com>
2024-03-07 14:58:30 -0300

    adicao do estado do modal

--- componentes/lista.js
+++ componentes/lista.js
@@ -2,6 +2,7 @@ import { View, SafeAreaView, StyleSheet, FlatList } from 'react-native';
 import { List, Text, IconButton, Divider, useTheme } from 'react-native-paper';
 import { useAppContext } from './provider';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useState } from 'react';
 
 /**
  * Este componente apresenta a lista de pessoas cadastradas.
@@ -16,6 +17,8 @@ export default function Lista() {
 
   const { colors, isV3 } = useTheme();
   const safeArea = useSafeAreaInsets();
+
+  const [modalVisible, setModalVisible] = useState(false);
 
   /**
    * Esta função é utilizada para renderizar um item da lista.
@@ -40,7 +43,8 @@ export default function Lista() {
         title={item.nome}
         style={selecionado && styles.item_selecionado}
         onPress={() => selecionarPessoa(item)}
-        right={selecionado && BotaoRemover}></List.Item>
+        right={selecionado && BotaoRemover}
+      ></List.Item>
     );
   };
   return (
@@ -76,7 +80,7 @@ export default function Lista() {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, minHeight:200 },
+  container: { flex: 1, minHeight: 200 },
   lista_mensagem_vazio: { marginHorizontal: 16 },
   cabecalho: {
     flex: 1,
